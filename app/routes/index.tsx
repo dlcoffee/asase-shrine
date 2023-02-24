9d35fec12e26fdcd59a8125b9a9c56e895bd0c45
daniel tea <daniel.l.tea@gmail.com>
2023-02-23 23:57:07 -0800

    update lists

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from '@remix-run/react'
 import { type LoaderArgs, json } from '@remix-run/cloudflare'
+import { type Day } from '~/data/items'
 
 import { db } from '~/utils/db.server'
 
@@ -15,13 +16,39 @@ export const loader = async (args: LoaderArgs) => {
 
 export default function Index() {
   const data = useLoaderData<typeof loader>()
+  const today = new Date()
+    .toLocaleDateString('en-US', { weekday: 'long' })
+    .toLowerCase() as Day
 
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold underline">Asase Shrine</h1>
+      <h2>Today: {today}</h2>
       <ul className="list-disc">
         {data.characters.map((character) => {
-          return <li key={character.id}>{character.name}</li>
+          const { id, name, talent_materials } = character
+          return (
+            <li key={id}>
+              <>
+                {name} |
+                {talent_materials.map((m) => {
+                  if (m.days?.includes(today)) {
+                    return (
+                      <span key={m.id} className="font-bold">
+                        {m.name}
+                      </span>
+                    )
+                  }
+
+                  return (
+                    <span key={m.id} className="font-normal">
+                      {m.name}
+                    </span>
+                  )
+                })}
+              </>
+            </li>
+          )
         })}
       </ul>
     </div>
